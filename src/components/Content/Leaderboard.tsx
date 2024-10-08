99ca5127a70fdcf7a62a6ac74ca90c72273b75d3
Martin Etchebarne <martinetchebarne829@gmail.com>
2024-10-08 16:41:51 -0300

    Fix leaderboard mobile height

--- src/components/Content/Leaderboard.tsx
+++ src/components/Content/Leaderboard.tsx
@@ -3,7 +3,7 @@ export default function Leaderboard() {
     return (
       <div className="flex justify-between items-center bg-white rounded-full pr-4">
         <div className="flex items-center gap-2">
-          <img className="bg-black h-12 w-12 rounded-full" src="" />
+          <img className="bg-black h-12 w-12 rounded-full" src="" alt="" />
           <div className="flex flex-col">
             <span className="font-semibold w-36 overflow-hidden text-ellipsis">martinetbn</span>
             <span className="text-[14px]">Page 0</span>
@@ -16,7 +16,7 @@ export default function Leaderboard() {
 
   return (
     <div className="w-full h-full flex items-center justify-center py-4 pt-12 px-2">
-      <div className="bg-[#E6CBAD] h-[75vh] lg:h-fit max-h-[570px] p-4 pt-6 rounded-[40px]">
+      <div className="bg-[#E6CBAD] h-[70vh] lg:h-fit max-h-[570px] p-4 pt-6 rounded-[40px]">
         <div className="bg-[#EFEFEF] h-full px-6 lg:p-8 py-8 rounded-[32px] relative flex flex-col gap-6">
           <img
             className="absolute h-[72px] top-[-60px] left-[50%] transform -translate-x-1/2"
